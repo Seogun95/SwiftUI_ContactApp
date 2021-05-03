e9f1d59f8bec79c58ef7a1dff791e95207b53960
SeonJungKim <ksj083895@gmail.com>
2021-05-03 14:28:45 +0500

    Initial Commit

---
+++ SwiftUI_ContactApp/SwiftUI_ContactAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ContactAppApp.swift
+//  SwiftUI_ContactApp
+//
+//  Created by 김선중 on 2021/05/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ContactAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
